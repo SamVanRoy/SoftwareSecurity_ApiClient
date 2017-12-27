25a5dff5811ca0faec1e32281d857d5e088d51f7
Unknown <sam.van.roy@student.ehb.be>
2017-12-27 18:00:28 +0100

    Test 2

---
+++ client/src/auth/auth0-variables-test.js
@@ -0,0 +1,7 @@
+export const AUTH_CONFIG = {
+  clientId: '5cBmuKA44szFJOOVZ2kuHG7GbClXrb8r',
+  domain: 'samvanroy.eu.auth0.com',
+  callbackUrl: 'http://localhost:8080/callback',
+  apiUrl: '{API_IDENTIFIER}'
+}
+
